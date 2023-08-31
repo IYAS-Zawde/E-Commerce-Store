98db00a26edda3f554b04b034fc70419d688534c
IYAS Zawde <iyas.zawde@gmail.com>
2023-08-31 14:51:50 +0300

    Adding Header
    In this step we add the header, which contain Store Name and Card button.
    Key-Points:
    - Use <mat-toolbar> tag
    - Adding Store Name
    - Add Card Button and use <mat-icon> tag and configure Badge for it.
    - Make the pop-up menu
    - Link the Card Button with the menu
    - Add more details to the card menu
    Issues we faced:
    - Even when I Used the Icons and checked for the spelling of them, they don't render. the problem was missing to import the link for google font (Material-Icons family) in the index.html.
    - There was a problem when the shopping icon clicked, the menu doesn't pop-up, after searching, the actual problem was a missing import which is "BrowserAnimationsModule" in the app.module.ts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatGridListModule } from 'node_modules old/@angular/material/grid-list';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from 'node_modules old/@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
-import { MatExpansionModule } from 'node_modules old/@angular/material/expansion';
-import { MatToolbarModule } from 'node_modules old/@angular/material/toolbar';
-import { MatBadgeModule } from 'node_modules old/@angular/material/badge';
-import { MatSidenavModule } from 'node_modules old/@angular/material/sidenav';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { HeaderComponent } from './components/header/header.component';
 
@@ -25,6 +25,7 @@ import { HeaderComponent } from './components/header/header.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     MatSidenavModule,
     MatGridListModule,
     MatMenuModule,
